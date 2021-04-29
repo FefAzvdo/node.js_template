4a3f99af66b523eb5fb6aab69a12b60a51666cc4
Fernando Augusto <wfernandoaugusto@gmail.com>
2021-04-28 21:34:00 -0300

    prettier configurado

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import routes from "./routes"
-import express from "express"
+import routes from './routes';
+import express from 'express';
 
 const app = express();
 
-app.listen(3333, () => console.log("Rodando na porta 3333"))
+app.listen(3333, () => console.log('Rodando na porta 3333'));
 
 app.use(routes);
